0c90126c1de8570a6eef23f6a78a6b38a56e291d
Jarrod McGee <jarrod.mcgee@ttu.edu>
2018-04-28 19:25:18 -0500

    Basic Demo
    Heatmap works just need to input data from DB

--- app/src/main/java/com/example/bmcge/databaseheatmap/MapsActivity.java
+++ app/src/main/java/com/example/bmcge/databaseheatmap/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.bmcge.databaseheatmap;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,6 +12,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Tile;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+import com.google.maps.android.heatmaps.WeightedLatLng;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -51,6 +62,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(NewYorkCity));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(9.85f));
         mMap.setMinZoomPreference(9.85f);
+        addHeatMapWeighted();
+    }
+    public void addHeatMapWeighted(){
+        List<WeightedLatLng> VioList = null;
+        //Set the list to our lat and longs with weight
+        try{
+            VioList = getViolationData(1);
+        }catch(JSONException e){
+            Toast.makeText(this, "Problem reading violations.", Toast.LENGTH_LONG).show();
+        }
+        HeatmapTileProvider mProvider = new HeatmapTileProvider.Builder()
+                .weightedData(VioList)
+                .build();
 
+        TileOverlay mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
+    private ArrayList<WeightedLatLng> getViolationData(int resource) throws JSONException{
+        ArrayList<WeightedLatLng> VioList = new ArrayList<WeightedLatLng>();
+        //Read from out data now
+        VioList.add(new WeightedLatLng(new LatLng(40.664077, -73.950132),2.0f));
+        VioList.add(new WeightedLatLng(new LatLng(40.631386, -74.026723),4.0f));
+        VioList.add(new WeightedLatLng(new LatLng(40.744054, -73.976719),1.0f));
+
+        return VioList;
     }
 }
